d373ab126f6abeeb83239ba2fdea757753cd375c
ali <alihamood96@gmail.com>
2025-01-05 18:02:33 -0800

    Adds default comments and an event listener for the submit form. The listener will check user input if it is below a limit, the input border is changed to red and an error message is displied, otherwise, it will add a new comment object and call a renderComments method. The renderComments method currently prints to the console.

---
+++ scripts/index-page.js
@@ -0,0 +1,91 @@
+const commentForm = document.getElementById('commentsForm');
+const commentSection = document.getElementById('commentsSection');
+
+const minUserNameLen = 3;
+const minCommentLen = minUserNameLen;
+const redBorderStyleClass = 'comments__form--error';
+
+const comments = [
+    {
+        userName: 'Victor Pinto',
+        comment: 'This is art. This is inexplicable magic expressed in the purest way, ' + 
+        'everything that makes up this majestic work deserves reverence. Let us appreciate' +
+        ' this for what it is and what it contains.',
+        commentDate: new Date('11/02/2023'),
+        rendered: false,
+    },
+
+    {
+        userName: 'Christina Cabrera',
+        comment: 'I feel blessed to have seen them in person. What a show! They were ' +
+        'just perfection. If there was one day of my life I could relive, this ' +
+        'would be it. What an incredible day.',
+        commentDate: new Date('10/28/2023'),
+        rendered: false,
+    },
+
+    {
+        userName: 'Isaac Tadesse',
+        comment: 'I can\'t stop listening. Every time I hear one of their songs - the vocals ' +
+        '- it gives me goosebumps. Shivers straight down my spine. What a ' +
+        'beautiful expression of creativity. Can\'t get enough.',
+        commentDate: new Date('10/20/2023'),
+        rendered: false,
+    },
+];
+
+function submitCommentHandler(event) {
+    event.preventDefault();
+
+    const userName = event.target.userName.value;
+    const userComment = event.target.comment.value;
+    const commentDate = new Date();
+
+    const userNameInput = event.target.userName;
+    userNameInput.classList.remove(redBorderStyleClass);
+    const commentInput = event.target.comment;
+    commentInput.classList.remove(redBorderStyleClass);
+
+    let invalidInput = false;
+    let errorMessage = '';
+
+    if (userName.length < minUserNameLen) {
+        userNameInput.classList.add(redBorderStyleClass);
+        invalidInput = true;
+        errorMessage += 'Invalid user name.\n'
+    }
+
+    if (userComment < minCommentLen) {
+        commentInput.classList.add(redBorderStyleClass);
+        invalidInput = true;
+        errorMessage += 'Invalid comment.\n'
+    }
+
+    if (invalidInput) {
+        alert(errorMessage);
+        return;
+    }
+
+    comments.push({
+        userName: userName,
+        comment: userComment,
+        commentDate: commentDate,
+    });
+
+    renderComments();
+}
+
+function renderComments() {
+    for (let comment of comments) {
+        if (!comment.rendered) {
+            console.log('User Name: ' + comment.userName);
+            console.log('User comment: ' + comment.comment);
+            console.log('Date: ' + comment.commentDate);
+            console.log('\n');
+            comment.rendered = true;
+        }
+    }
+}
+
+commentForm.addEventListener('submit', submitCommentHandler);
+renderComments();
